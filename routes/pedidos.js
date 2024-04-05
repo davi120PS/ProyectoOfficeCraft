3216fa3c70c98ad3123a302bea376f511532b1b8
davi120ps <santos111@outlook.com>
2024-04-04 23:13:59 -0500

    Deberia funcionar

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -8,7 +8,7 @@ const {connection} = require("../config/config.db");
 
 //Metodo para traer todos los pedidos de la tabla pedidos en bd
 const getPedido = (request,response) => {
-    connection.query("SELECT p.PedidoID,p.FechaPedido,p.Estado,cl.Nombre FROM pedidos p INNER JOIN clientes cl WHERE p.ClienteID = cl.ClienteID",
+    connection.query("SELECT p.PedidoID, p.FechaPedido, p.Estado, cl.Nombre FROM pedidos p INNER JOIN clientes cl WHERE p.ClienteID = cl.ClienteID",
     (error,results)=>{
         if(error)
         throw error;
@@ -17,13 +17,25 @@ const getPedido = (request,response) => {
 };
 app.route("/pedidos").get(getPedido);
 
+//Listado de pedidos y a que cliente pertenece
+const getPedidoCliente = (request,response) => {
+    connection.query('SELECT cl.Nombre FROM pedidos as p'+
+                    ' INNER JOIN clientes as cl ON p.ClienteID = cl.ClienteID',
+    (error,results)=>{
+        if(error)
+            throw error;
+        response.status(200).json(results);
+    });
+};
+app.route("/pedidoscliente").get(getPedidoCliente);
+
 /*Metodo para crear o actualizar un cliente*/
 const postPedido = (request, response) => {
-    const {action,id,cliente,fecha,estado} = request.body;
+    const {action,id,cliente,fecha,estado,PedidoID} = request.body;
     //console.log(action);return false;
     if(action == "insert"){
         connection.query("INSERT INTO pedidos (ClienteID, FechaPedido, Estado) VALUES (?,?,?)", 
-        [cliente, fecha,estado],
+        [cliente,fecha,estado],
         (error, results) => {
             if(error)
                 throw error;
@@ -33,8 +45,8 @@ const postPedido = (request, response) => {
 //*En caso de añadir un ID existente al agregar se actualiza el pedido seleccionado*/
     else{
         //console.log(action);return false;
-        connection.query("UPDATE pedidos SET ClienteID=?, FechaPedido =?, Estado = ? WHERE PedidoID = ?", 
-        [cliente, fecha,estado,id],
+        connection.query("UPDATE pedidos SET ClienteID=?, FechaPedido =?, Estado = ? WHERE PedidoID = "+PedidoID+"", 
+        [cliente,fecha,estado,id],
         (error, results) => {
             if(error)
                 throw error;
@@ -43,6 +55,18 @@ const postPedido = (request, response) => {
     }
 };
 app.route("/pedidos").post(postPedido);
+
+const getPedidoId = (request,response) => {
+    const id = request.params.id;
+    connection.query("SELECT p.*, cl.Nombre AS cliente, p.FechaPedido AS fechapedido, p.Estado AS estado FROM pedidos p LEFT JOIN clientes cl ON p.ClienteID = cl.ClienteID WHERE p.ClienteID = ?",
+    [id],
+    (error,results)=>{
+        if(error)
+            throw error;
+        response.status(200).json(results);
+    });
+};
+app.route("/pedidos/:id").get(getPedidoId);
 
 //Servicio para eliminar un cliente
 const delPedido = (request,response)=>{
