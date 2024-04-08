33e36cdef0d81012f65678075486648452abb546
davi120ps <santos111@outlook.com>
2024-04-08 10:06:11 -0500

    query PEDIDOS completa

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -6,9 +6,12 @@ dotenv.config();
 //conexion con la base de datos
 const {connection} = require("../config/config.db");
 
+// Middleware para parsear el cuerpo de la solicitud en formato JSON
+app.use(express.json());
+
 //Metodo para traer todos los pedidos de la tabla pedidos en bd
 const getPedido = (request,response) => {
-    connection.query("SELECT p.PedidoID,p.FechaPedido,p.Estado,cl.Nombre FROM pedidos p INNER JOIN clientes cl WHERE p.ClienteID = cl.ClienteID",
+    connection.query("SELECT p.PedidoID, p.FechaPedido, p.Estado,cl.Nombre AS Cliente FROM pedidos p INNER JOIN clientes cl ON p.ClienteID = cl.ClienteID",
     (error,results)=>{
         if(error)
         throw error;
@@ -19,9 +22,9 @@ app.route("/pedidos").get(getPedido);
 
 /*Metodo para crear o actualizar un cliente*/
 const postPedido = (request, response) => {
-    const {action,id,cliente,fecha,estado} = request.body;
+    const {action,id,cliente,fechapedido,estado,PedidoID} = request.body;
     //console.log(action);return false;
-    if(action == "insert"){
+    if(action === "insert"){
         connection.query("INSERT INTO pedidos (ClienteID, FechaPedido, Estado) VALUES (?,?,?)", 
         [cliente, fecha,estado],
         (error, results) => {
@@ -33,8 +36,8 @@ const postPedido = (request, response) => {
 //*En caso de añadir un ID existente al agregar se actualiza el pedido seleccionado*/
     else{
         //console.log(action);return false;
-        connection.query("UPDATE pedidos SET ClienteID=?, FechaPedido =?, Estado = ? WHERE PedidoID = ?", 
-        [cliente, fecha,estado,id],
+        connection.query("UPDATE pedidos SET ClienteID=?, fechapedido =?, Estado = ? WHERE PedidoID = "+PedidoID+"", 
+        [cliente,fechapedido,estado,id],
         (error, results) => {
             if(error)
                 throw error;
@@ -46,7 +49,7 @@ app.route("/pedidos").post(postPedido);
 
 const getPedidoId = (request,response) => {
     const id = request.params.id;
-    connection.query("SELECT p.*, cl.Nombre, cl.ClienteID AS cliente, p.FechaPedido AS fechapedido, p.Estado AS estado, FROM pedidos p LEFT JOIN clientes cl ON p.ClienteID = cl.ClienteID WHERE p.PedidoID = ?",
+    connection.query("SELECT p.*, cl.Nombre, cl.ClienteID AS cliente, p.FechaPedido AS fechapedido, p.Estado AS estado FROM pedidos p LEFT JOIN clientes cl ON p.ClienteID = cl.ClienteID WHERE p.PedidoID = ?",
     [id],
     (error,results)=>{
         if(error)
